1b08e6e22f1c6d114af078658a03b14bd76bf000
PoznyakM <vash.lavashik@gmail.com>
2022-05-31 20:21:08 +0300

    v2.2.9

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser(/*secret дописать*/));
-  app.enableCors();
+  app.enableCors({origin: 'http://localhost:3000'});
   const configService: ConfigService = app.get(ConfigService);
   const config = new DocumentBuilder()
       .setTitle('ARTART REST API')
